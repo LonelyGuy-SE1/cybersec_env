37aa155ff2879f99a7d936a4ee1b4e2855dfbe8d
LonelyGuy-SE1 <brawlers.assemble@gmail.com>
2026-04-20 18:03:47 +0530

    initial commit

---
+++ cybersec/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Cybersec Environment."""
+
+from .client import CybersecEnv
+from .models import CybersecAction, CybersecObservation
+
+__all__ = [
+    "CybersecAction",
+    "CybersecObservation",
+    "CybersecEnv",
+]
---
+++ cybersec/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Cybersec environment server components."""
+
+from .cybersec_environment import CybersecEnvironment
+
+__all__ = ["CybersecEnvironment"]
